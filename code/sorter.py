7313a29b0778ef00d8e1c06c884ce04ea5c7789b
Josh Borrow <joshua.borrow@durham.ac.uk>
2016-01-24 15:04:33 +0000

    added all initial functions for sorting, no saving yet though, no testing

--- code/sorter.py
+++ code/sorter.py
@@ -13,14 +13,12 @@ class dataset(object):
         self.union_delim = "\t"
         self.total_url = "../data/all_rawdata.csv"
         self.total_delim = "\t"
-        self.union_rawdata
-        self.all_rawdata
-        self.names
-        self.mu
-        self.z
-        self.z_err
-        self.ra
-        self.dec
+        self._get_raw()
+        self._get_names()
+        self._get_mu()
+        self._get_mu_err()
+        self._get_z()
+        self._get_ra_dec()
 
         return
 
@@ -48,3 +46,39 @@ class dataset(object):
         self.mu = self.union_rawdata[:,2]
 
         return
+
+
+    def _get_mu_err(self):
+        self.mu_err = self.union_rawdata[:,3]
+
+        return
+
+
+    def __get_name_indicies(self):
+        """Finds the row numbers where names appear from U2.1 in all dataset so
+        we can extract the ra/dec"""
+        total_rawnames = self.union_rawdata[:,0]
+        indicies = np.array_like(self.names)
+
+        for i in range(len(names)):
+            indicies[i] = total_rawnames.index(names[i])
+
+        return indicies
+
+
+    def _get_ra_dec(self):
+        self.ra = np.array_like(names)
+        self.dec = np.array_like(names)
+
+        indicies = __get_name_indicies()
+
+        for i in range(len(self.ra)):
+            self.ra[i] = self.union_rawdata[indicies[i],3]
+            self.dec[i] = self.union_rawdata[indicies[i],4]
+
+        return
+
+
+if __name__ == "__main__":
+    data = dataset()
+    print(data.ra())
